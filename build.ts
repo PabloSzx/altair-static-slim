2cf94c4b1240032ad3176172eac85543e783f309
PabloSzx <pablosaez1995@gmail.com>
2021-10-20 19:42:24 -0300

    sync 4.1.0

---
+++ build.ts
@@ -0,0 +1,11 @@
+import { build } from "esbuild";
+
+await build({
+  entryPoints: ["src/index.ts"],
+  outfile: "build/index.js",
+  platform: "node",
+  target: "node13.2",
+  format: "cjs",
+  bundle: true,
+  minify: true,
+});
