ecfe545a451bfbc3481b9f02c663cbc9c2b270e0
Pablo Sáez <pablosaez1995@gmail.com>
2022-07-15 21:01:20 -0400

    fix dist dir

--- copy-dist.ts
+++ copy-dist.ts
@@ -2,7 +2,7 @@ import { copy } from "fs-extra";
 import { join } from "path";
 import { getDistDirectory } from "altair-static";
 
-copy(getDistDirectory(), join(__dirname, "dist")).catch((err) => {
+copy(getDistDirectory(), join(__dirname, "build/dist")).catch((err) => {
   console.error(err);
   process.exit(1);
 });
